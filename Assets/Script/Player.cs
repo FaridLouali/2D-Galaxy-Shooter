2989fe5c845c49e0e87fc6fdfca114afedc9f145
Farid Louali <farid-louali@creativenetmediaa.com>
2021-04-06 23:15:54 -0400

    Created Player and attached c# script to it

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //take the current position = new position (0,0,0) 
+        transform.position = new Vector3(0, 0, 0);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(Vector3.right * _speed * Time.deltaTime);
+        
+    }
+}
