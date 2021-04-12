a19d91de7ba97c8400e5d2803f07c7a22de49f0c
Farid Louali <farid-louali@creativenetmediaa.com>
2021-04-11 23:42:35 -0400

    Added player movements

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _speed = 3.5f;
 
+   
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,8 +20,34 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+    }
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.right * _speed * Time.deltaTime);
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+
         
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+
+        if (transform.position.x > 11.2f)
+        {
+            transform.position = new Vector3(-11.2f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11.2f)
+        {
+            transform.position = new Vector3(11.2f, transform.position.y, 0);
+        }
+
     }
 }
