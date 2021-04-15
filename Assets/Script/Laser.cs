820687aefc7ecd89a4b186a2dc0df0265d549d20
Farid Louali <farid-louali@creativenetmediaa.com>
2021-04-14 22:10:35 -0400

    Shooting: Laser Setup and Cool down System

---
+++ Assets/Script/Laser.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float laserSpeed = 10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * laserSpeed * Time.deltaTime);
+
+        if(transform.position.y > 7f )
+            {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,22 +6,31 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 3.5f;
-
-   
-
+    [SerializeField]
+    private GameObject laserPrefab;
+    //variables for the cooldown system
+    public float fireRate = 0.5f;
+    private float _canFire = -1f; // start timing for the next firing.
+    
     // Start is called before the first frame update
     void Start()
     {
-        //take the current position = new position (0,0,0) 
-        transform.position = new Vector3(0, 0, 0);
-       
+        transform.position = new Vector3(0, 0, 0);   
     }
 
     // Update is called once per frame
     void Update()
     {
         CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            FireLaser();
+        }
+           
+
     }
+
     void CalculateMovement()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
@@ -30,15 +39,9 @@ public class Player : MonoBehaviour
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
         transform.Translate(direction * _speed * Time.deltaTime);
 
-        
-        if (transform.position.y > 0)
-        {
-            transform.position = new Vector3(transform.position.x, 0, 0);
-        }
-        else if (transform.position.y <= -3.8f)
-        {
-            transform.position = new Vector3(transform.position.x, -3.8f, 0);
-        }
+        //We clamping the values of Y between 0 and 3.8F, using Mathf.clamp() to optimize the code.
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y,-3.8f,0), 0);
+
 
         if (transform.position.x > 11.2f)
         {
@@ -50,4 +53,11 @@ public class Player : MonoBehaviour
         }
 
     }
+
+    void FireLaser()
+        {
+            _canFire = Time.time + fireRate;
+            Instantiate(laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+        }
+    
 }
